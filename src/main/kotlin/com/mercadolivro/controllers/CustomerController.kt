ff07289374464b84808f163a784fd399bd7455ce
MatheusMoraes66 <matheus.csharp@hotmail.com>
2022-10-20 21:05:42 -0300

    Post route creation and get adjustment

--- src/main/kotlin/com/mercadolivro/controllers/CustomerController.kt
+++ src/main/kotlin/com/mercadolivro/controllers/CustomerController.kt
@@ -1,4 +1,28 @@
 package com.mercadolivro.controllers
 
+import com.mercadolivro.dto.PostCustomerRequest
+import com.mercadolivro.models.CustomerModel
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+import java.net.http.HttpResponse
+
+@RestController
+@RequestMapping("/customer")
 class CustomerController {
+
+    @GetMapping
+    fun getCustomer(): CustomerModel{
+        return CustomerModel("1", "matheus","matheus@email.com")
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    fun create(@RequestBody customer: PostCustomerRequest){
+        println(customer)
+    }
 }
