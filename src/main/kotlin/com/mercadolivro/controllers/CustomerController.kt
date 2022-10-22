a18298b30f5d03ca69c4246632c21de6895838d5
MatheusMoraes66 <matheus.csharp@hotmail.com>
2022-10-22 03:25:14 -0300

    getAll get a new parameter

--- src/main/kotlin/com/mercadolivro/controllers/CustomerController.kt
+++ src/main/kotlin/com/mercadolivro/controllers/CustomerController.kt
@@ -4,12 +4,14 @@ import com.mercadolivro.dto.PostCustomerRequest
 import com.mercadolivro.dto.PutCustomerRequest
 import com.mercadolivro.models.CustomerModel
 import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import java.net.http.HttpResponse
@@ -22,7 +24,10 @@ class CustomerController {
     val customers = mutableListOf<CustomerModel>()
 
     @GetMapping
-    fun getAll(): List<CustomerModel>{
+    fun getAll(@RequestParam name: String?): List<CustomerModel>{
+        name?.let {
+            return customers.filter { it.name.contains(name, true) }
+        }
         return customers
     }
 
@@ -53,5 +58,11 @@ class CustomerController {
             it.email = customer.email
             it.name = customer.name
         }
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun delete(@PathVariable id: String){
+        customers.removeIf{ it.id == id}
     }
 }
