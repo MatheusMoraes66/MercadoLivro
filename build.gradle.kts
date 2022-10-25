4570b46d9a2de2c8a64fba13b6193454cbd3a7db
MatheusMoraes66 <matheus.csharp@hotmail.com>
2022-10-25 20:28:14 -0300

    adjustment in flyway dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,8 +22,8 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("org.flywaydb:flyway-core:9.5.1")
-
+	implementation("org.flywaydb:flyway-core:9.4.0")
+	implementation("org.flywaydb:flyway-mysql")
 	runtimeOnly("mysql:mysql-connector-java")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
